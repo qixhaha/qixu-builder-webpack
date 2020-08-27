68c894fc208eb13a1ae68ff3157cb135c072eea9
qixuhaha <qixu17526601440@163.com>
2020-08-27 19:58:35 +0800

    修改单元测试

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,7 +7,7 @@ describe('webpack.base.js test case',()=>{
     const baseConfig = require('../../lib/webpack.prod');
     console.log(baseConfig);
     it('entry',()=>{
-        assert.equal(baseConfig.entry.index,'D:/大前端学习资源/webpack/builder-webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search,'D:/大前端学习资源/webpack/builder-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js')>-1,true)
+        assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js')>-1,true)
     })
 })
